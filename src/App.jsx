8d1a96b4ceb78674840d9c691535198fb1e004ac
Prashant Balappa Kumbar <pkumbar@vaeso.com>
2026-04-14 07:29:29 +0530

    tailwind and daisyui configuration error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Body from "./components/Body";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
-import Feed from "./components/Feed";
-import Connections from "./components/Connections";
-import Requests from "./components/Requests";
-import Premium from "./components/Premium";
-import Chat from "./components/Chat";
 
 function App() {
   return (
     <>
-      <Provider store={appStore}>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="/" element={<Body />}>
-              <Route path="/" element={<Feed />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/connections" element={<Connections />} />
-              <Route path="/requests" element={<Requests />} />
-              <Route path="/premium" element={<Premium />} />
-              <Route path="/chat/:targetUserId" element={<Chat />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <button className="btn btn-primary">DaisyUI Button</button>
     </>
   );
 }
